c861566926cc42557b64c4be295de59fa1c885ed
rosydiazl <rosyosmediazl@gmail.com>
2021-09-18 14:24:44 -0400

    Add the URL for profiles and userprofile. They are differnt now because there are two index requests instead of one

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views.profile_views import Profiles, ProfileDetail
+from .views.profile_views import Profiles, ProfileDetail, NewProfile
 from .views.likes_views import Like, LikeDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
   	# Restful routing
-    path('profiles/', Profiles.as_view(), name='profiles'),
-    path('profiles/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
+    path('profiles/', NewProfile.as_view(), name='profiles'),
     path('userprofile/', Profiles.as_view(), name='userprofile'),
+    path('userprofile/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
     path('likes/', Like.as_view(), name='likes'),
     path('likes/<int:pk>/', LikeDetail.as_view(), name='like_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
