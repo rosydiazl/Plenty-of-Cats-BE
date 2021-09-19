aad93202e06bccb6d85f12fdbfabc9b4a8f533e2
rosydiazl <rosyosmediazl@gmail.com>
2021-09-19 11:43:05 -0400

    Change the curl script for show likes. Not working as expected yet

--- curl-scripts/likes/show.sh
+++ curl-scripts/likes/show.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# TOKEN="c1bd351f83f4ff039f0f399d23bdf05b76cbf9d7" ID="3" sh curl-scripts/profile/show.sh
+# TOKEN="c1bd351f83f4ff039f0f399d23bdf05b76cbf9d7" ID="1" sh curl-scripts/profile/show.sh
 
 curl "http://localhost:8000/likes/${ID}/" \
   --include \
