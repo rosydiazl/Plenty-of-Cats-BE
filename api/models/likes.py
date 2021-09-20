0288da30316059fd98539d5ce876d06664bf36a2
rosydiazl <rosyosmediazl@gmail.com>
2021-09-20 15:10:08 -0400

    Change Like to Likes since its the appropiate name

--- api/models/likes.py
+++ api/models/likes.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Likes(models.Model):
     profile_id = models.ForeignKey('Profile', on_delete=models.CASCADE)
     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.created
