02fe51c98516112ab12a409f13b64a0e77c8b0e0
rosydiazl <rosyosmediazl@gmail.com>
2021-09-20 22:01:16 -0400

    Add owner to the INDEX request for likes so users can see only the profiles they have liked

--- api/models/likes.py
+++ api/models/likes.py
@@ -3,10 +3,22 @@ from django.contrib.auth import get_user_model
 
 
 class Likes(models.Model):
-    profile_id = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
+  name = models.CharField(max_length=100)
+  age = models.IntegerField()
+  breed = models.CharField(
+      max_length=100
+  )
+  bio = models.TextField(max_length=200)
+  profile_id = models.ForeignKey('Profile', on_delete=models.CASCADE)
+  user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+  created = models.DateTimeField(auto_now_add=True)
+  owner = models.ForeignKey(
+      get_user_model(),
+      on_delete=models.CASCADE,
+      related_name='current_owner'
+  )
 
 
-    def __str__(self):
+
+  def __str__(self):
         return self.created
